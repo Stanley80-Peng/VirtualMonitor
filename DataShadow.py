f1700a98870645f7528358990d3d276e5daedfb6
Stanley80-Peng <stanley80@yeah.net>
2020-08-12 17:47:16 +0800

    commit

--- DataShadow.py
+++ DataShadow.py
@@ -8,25 +8,26 @@ import multiprocessing as mp
 class DataShadow(object):
     def __init__(self, path):
         proc_dir(path)
+        delete_existed_files()
+
+
+def delete_existed_files():
+    if not os.path.isdir('positions') and os.path.isdir('positions/shadow'):
+        return
+    files = os.listdir('positions/shadow')
+    for file in files:
+        os.remove('positions/shadow/' + str(file))
 
 
 def proc_dir(path):
-    p = Pool(mp.cpu_count())
-    DIR = os.listdir(path)
-    for dir_in in DIR:
-        if os.path.isdir(path + '/' + str(dir_in)):
-            files = os.listdir(path + '/' + str(dir_in))
-            for file in files:
-                with open(path + '/' + str(dir_in) + '/' + file, 'rb') as f:
-                    if f.name.split('/')[-1].find('slam') + 1:
-                        proc_slam(f, str(dir_in), )
-                    # p.apply_async(proc_slam, args=(f, str(dir),))
-    p.close()
-    p.join()
+    files = os.listdir(path)
+    for file in files:
+        with open(path + '/' + str(file) + '/' + file, 'rb') as f:
+            if f.name.split('/')[-1].find('slam') + 1:
+                proc_slam(f, path, )
 
 
 def proc_slam(in_file, dir_name):
-    first_flag = True
     if not os.path.exists('./positions'):
         os.mkdir('./positions')
         os.mkdir('./positions/shadow')
@@ -36,14 +37,10 @@ def proc_slam(in_file, dir_name):
     out_file = open('./positions/shadow/' + str(dir_name) + '-slam.csv', 'w', encoding='UTF-8')
     save_sec = 0
     while True:
-        temp = in_file.read(8)
-        if not temp:
+        _time = in_file.read(8)
+        if not _time:
             break
-        '''unpack1 = struct.unpack('=I', struct.pack('4B', *temp))
-        # #print('%u,' % unpack, end='')  #
-        temp = in_file.read(4)
-        unpack2 = struct.unpack('=I', struct.pack('4B', *temp))'''
-        unpack_time = struct.unpack('II', temp)
+        unpack_time = struct.unpack('II', _time)
         sub_sec = int(str(unpack_time[1])[0:2])
         sub_sec -= sub_sec % 3
         now_sec = str(unpack_time[0]) + '.' + str(sub_sec)
@@ -53,12 +50,12 @@ def proc_slam(in_file, dir_name):
         save_sec = now_sec
         out_file.write('%s,' % now_sec)
         # #print('%u,' % unpack, end='')  #
-        for i in range(7):
-            temp = in_file.read(4)
-            unpack = struct.unpack('=f', struct.pack('4B', *temp))
-            out_file.write('%.4f,' % unpack[0])
-            # #print('%f,' % unpack, end='')  #
-        out_file.write('\n')
+        _float = in_file.read(4 * 7)
+        unpack_float = struct.unpack('f' * 7, _float)
+        for i in range(6):
+            out_file.write('%.4f' % unpack_float[i])
+        out_file.write('%.4f\n' % unpack_float[6])
+        # #print('%f,' % unpack, end='')  #
         # #print('\n')  #
     in_file.close()
     out_file.close()
