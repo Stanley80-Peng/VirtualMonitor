bc30b1d3e0e81aace12e7dc56323609d1b799f24
Stanley Peng <Stanley80@yeah.net>
2020-08-17 10:44:54 +0800

    find log optimized

--- AnimatePlanner.py
+++ AnimatePlanner.py
@@ -180,9 +180,9 @@ class AnimatePlanner(object):
             files = sorted(os.listdir(log_path))
             time_tup = str(self.time_list[self.end_index]).split(':')
             if identifier == 'planner' or identifier == 'slamNode' or identifier == 'robotcomm':
-                date_time = str(self.day_num) + ' ' + time_tup[0] + ':' + time_tup[1]
+                date_time = str(self.day_num) + ' ' + time_tup[0] + ':' + time_tup[1] + ':' + time_tup[2][0:1]
             else:
-                date_time = str(self.day_num) + ' ' + time_tup[0]
+                date_time = str(self.day_num) + ' ' + time_tup[0] + ':' + time_tup[1][0:1]
             # print(date_time)
             for file in files:
                 if re.search('INFO', str(file)) and re.search(identifier, str(file)) and re.search('log', str(file)):
